f211ca3406ad3ec0120a5c8f4c85836a42090e62
max <max.sjolinder@gmail.com>
2018-03-22 18:52:43 +0100

    Added empty solution.

---
+++ Insynsregistret.SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Insynsregistret.Api;
+
+namespace Insynsregistret.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
