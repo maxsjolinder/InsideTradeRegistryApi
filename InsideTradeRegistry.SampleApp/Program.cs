e77e7bbd345fd77ca7906a19c8b2fee1bb8d8d59
Max sjolinder <max.sjolinder@gmail.com>
2018-04-09 21:03:03 +0200

    Renamed project from Insynsregistret to InsideTradeRegistry

---
+++ InsideTradeRegistry.SampleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using InsideTradeRegistry.Api;
+using System.Linq;
+
+namespace InsideTradeRegistry.SampleApp
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {           
+            IInsideTradeRegistryApi api = new InsideTradeRegistryApi();
+
+            var transactions = api.GetInsideTradeTransactionsAsync(new SearchQuery ( issuer:"Investor", person: "Johan Forssell")).GetAwaiter().GetResult();
+        }
+    }
+}
